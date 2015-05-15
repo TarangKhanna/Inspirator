39ba8a46bcdd400b82b0f610f191962da0f41d42
Tarang Khanna <khanna17@purdue.edu>
2015-05-15 14:33:35 -0400

    New ViewController

---
+++ WorkoutMotivation/NavigationControl.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationControl.swift
+//  WorkoutMotivation
+//
+//  Created by Tarang khanna on 5/15/15.
+//  Copyright (c) 2015 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+
+class NavigationControl: UINavigationController, UIViewControllerTransitioningDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //let navBar = self.navigationController!.navigationBar
+        //navBar.barTintColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        //navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        //self.view.backgroundColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        // Status bar white font
+        self.navigationBar.barTintColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        self.navigationBar.tintColor = UIColor.greenColor()
+    }
+}
