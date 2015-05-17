900a9fc2a7248300a1f068c7d2628497f9f7a28b
Tarang Khanna <khanna17@purdue.edu>
2015-05-17 03:35:50 -0400

    Randomized Pictures
    UIImage and rand func

--- WorkoutMotivation/WeightsVC.swift
+++ WorkoutMotivation/WeightsVC.swift
@@ -13,10 +13,12 @@ class WeightsVC: UIViewController {
     let q1 = WeigthsQuotes()
     
     @IBOutlet weak var Qlabel: UILabel!
+    @IBOutlet var ImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
         Qlabel.text = q1.getQuote()
         Qlabel.textColor = UIColor.greenColor()
+        ImageView.image = UIImage(named: "Weights\(arc4random_uniform(10)).png")
     }
 }
