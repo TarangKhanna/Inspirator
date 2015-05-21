52428aef8279c3da92a96c0f86db1e175bedccf8
Tarang Khanna <khanna17@purdue.edu>
2015-05-21 18:05:55 -0400

    loginVC
    added web views to motivational content

---
+++ WorkoutMotivation/loginVC.swift
@@ -0,0 +1,13 @@
+//
+//  loginVC.swift
+//  WorkoutMotivation
+//
+//  Created by Tarang khanna on 5/21/15.
+//  Copyright (c) 2015 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+}
