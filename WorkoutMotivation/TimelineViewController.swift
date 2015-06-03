4144bebeaf8090b6c6d7d83ad8087501c832f426
Tarang Khanna <khanna17@purdue.edu>
2015-06-04 03:48:30 +0530

    delete

--- WorkoutMotivation/TimelineViewController.swift
+++ WorkoutMotivation/TimelineViewController.swift
@@ -84,6 +84,7 @@ class TimelineViewController : UIViewController, UITableViewDelegate, UITableVie
         
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false
@@ -234,6 +235,16 @@ class TimelineViewController : UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userArray.count
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
