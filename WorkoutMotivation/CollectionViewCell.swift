b5e8448c06f7732a21bcc7e5fe9c65e606d5a1da
Tarang Khanna <khanna17@purdue.edu>
2015-06-06 10:49:20 +0530

    collection view

---
+++ WorkoutMotivation/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  AutomatePortal
+//
+//  Created by Tarang Khanna on 2/28/15.
+//  Copyright (c) 2015 Thacked. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var Image: UIImageView!
+}
